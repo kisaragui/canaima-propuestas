0515e0e30b400c486da4c6390bfe3372059eb765
Noel  Álvarez <njesusas@gmail.com>
2016-09-01 11:44:12 -0400

    Revert "Primera fase del sistema de postulacion-CRUD"
    This reverts commit 03a6be1233c1dd4682f87d90f88879eb9103249f.

--- CanaimaPropuestas/wsgi.py
+++ CanaimaPropuestas/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for canaima_propuestas project.
+WSGI config for CanaimaPropuestas project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "canaima_propuestas.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CanaimaPropuestas.settings")
 
 application = get_wsgi_application()
