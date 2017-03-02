ae03276eef18f99d4238ae8ea2516a38250aa76b
Noel Alvarez <njesusas@gmail.com>
2017-03-02 11:00:31 -0400

    --- probando entorno de produccion (tentativo 1)---
    Se agregaron los siguientes modificaciones y nuevas archivos:
    
    static -> son los archivos estaticos para  el entorno de produccion
    debian -> archivo debian para el empaquetamiento
    gunicorn_start.sh -> script para correr automantico gunicorn
    canaima_propuestas_ngnix.conf -> archivo de configuracion de nginx

--- canaima_propuestas/canaima_propuestas/settings.py
+++ canaima_propuestas/canaima_propuestas/settings.py
@@ -113,19 +113,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-ve'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False  
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "estilos"),)
 
